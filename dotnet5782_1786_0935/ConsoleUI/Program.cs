4476c3b64c57349def5bfacb62ee6eec8ff92bd6
Eliana Grajower <grajower@g.jct.ac.il>
2021-10-29 16:24:07 +0300

    change names+ moves things to main

---
+++ dotnet5782_1786_0935/ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using IDAL.DO;
+using DAL;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //static DalObject.DalObject Data;
+        static void Main(string[] args)
+        {
+            /*static*/ DalObject.DalObject Data;
+
+            Data = new DalObject.DalObject();
+            Console.WriteLine("enter a ststions id: ");
+            int stationId = int.Parse(Console.ReadLine());
+            Console.WriteLine(Data.PrintStation(stationId));
+
+            Console.WriteLine("enter a drones id: ");
+            int droneId = int.Parse(Console.ReadLine());
+            Console.WriteLine(Data.PrintDrone(droneId));
+
+            Console.WriteLine("enter a customer id: ");
+            int customerId = int.Parse(Console.ReadLine());
+            Console.WriteLine(Data.PrintCustomer(customerId));
+
+            Console.WriteLine("enter a parcel id: ");
+            int parcelId = int.Parse(Console.ReadLine());
+            Console.WriteLine(Data.PrintParcel(parcelId));
+
+            //  IDAL.DO.BaseStation baseStation = new IDAL.DO.BaseStation();
+            //   Console.WriteLine(baseStation);
+        }
+    }
+}
