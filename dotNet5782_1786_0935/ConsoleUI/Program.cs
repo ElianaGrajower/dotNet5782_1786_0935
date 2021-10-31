a387a7069645c1fe4f7217460b51d66d4da02606
adinaeschulman <aschulma@g.jct.ac.il>
2021-10-31 16:12:02 +0200

    dont know

--- dotNet5782_1786_0935/ConsoleUI/Program.cs
+++ dotNet5782_1786_0935/ConsoleUI/Program.cs
@@ -21,15 +21,100 @@ namespace ConsoleUI
             choice = int.Parse(Console.ReadLine());
             while (choice != 5)
             {
-                switch(choice)
+                switch (choice)
                 {
                     case 1:
                     case 2:
                     case 3:
+                        {
+                            char Choice_Print;
+                            int Id;
+                            Console.WriteLine("Enter choice you want to print:");
+                            Console.WriteLine("A- charge station");
+                            Console.WriteLine("B- drone");
+                            Console.WriteLine("C- customer");
+                            Console.WriteLine("D- parcel");
+                            Choice_Print = char.Parse(Console.ReadLine());
+                            Console.Write("Enter ");
+                            if (Choice_Print == 'A')
+                                Console.Write("charge Station ");
+                            if (Choice_Print == 'b')
+                                Console.Write("drone ");
+                            if (Choice_Print == 'C')
+                                Console.Write("customer ");
+                            if (Choice_Print == 'D')
+                                Console.Write("pacel ");
+                            Console.WriteLine("id");
+                            Id = int.Parse(Console.ReadLine());
+
+                            switch (Choice_Print)
+                            {
+                                case 'A':
+                                    {
+                                        Data.PrintStation(Id);
+                                        break;
+                                    }
+                                case 'B':
+                                    {
+                                        Data.PrintDrone(Id);
+                                        break;
+                                    }
+                                case 'C':
+                                    {
+                                        Data.PrintCustomer(Id);
+                                        break;
+                                    }
+                                case 'D':
+                                    {
+                                        Data.PrintParcel(Id);
+                                        break;
+                                    }
+                                default: break;
+
+                            }
+                            break;
+                        }
+
                     case 4:
-                    default: Console.WriteLine("ERROR INVALID CHOICE");
+                        {
+                            char Choice_Print;
+                            Console.WriteLine("Enter choice you want to print:");
+                            Console.WriteLine("A- charge station list");
+                            Console.WriteLine("B- drone list");
+                            Console.WriteLine("C- customer list");
+                            Console.WriteLine("D- parcel list");
+                            Choice_Print = char.Parse(Console.ReadLine());
+                            switch (Choice_Print)
+                            {
+                                case 'A':
+                                    {
+                                        Data.printStationsList();
+                                        break;
+                                    }
+                                case 'B':
+                                    {
+                                        Data.printDronesList();
+                                        break;
+                                    }
+                                case 'C':
+                                    {
+                                        Data.printCustomersList();
+                                        break;
+                                    }
+                                case 'D':
+                                    {
+                                        Data.printParcelsList();
+                                        break;             
+                                    }
+                                default:
+                                    break;
+                            }
+                            break;
+                        }
+                    default:
+                        Console.WriteLine("ERROR INVALID CHOICE");
                         break;
-                    
+                
                 }
                 Console.WriteLine("Choose from the following options:");
                 Console.WriteLine("1- To add new item");
