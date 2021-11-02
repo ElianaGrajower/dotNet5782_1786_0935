5c3aced253708831125e760884bc41b9308a5ac1
adinaeschulman <aschulma@g.jct.ac.il>
2021-11-02 19:24:07 +0200

    fixed minor functions

--- dotNet5782_1786_0935/DAL/DalObject.cs
+++ dotNet5782_1786_0935/DAL/DalObject.cs
@@ -89,6 +89,9 @@ namespace DAL
             }
             public string deliverParcel(Customer customer, Parcel parcel, int priorityLevel) //matches up parcel with buyer
             {
+                
+                if(customer.CustomerId==0)
+                    return "Your request could not be completed";
                 string complete = "Your request was completed successfully";
                 parcel.TargetId = customer.CustomerId;
                 parcel.Delivered = DateTime.Now;
@@ -116,6 +119,7 @@ namespace DAL
             {
                 string complete = "Your request was completed successfully";
                 drone.Status = DroneStatuses.maintenance;
+                drone.Battery = 0;
                 Station station = DataSource.StationList.Find(temp => (temp.Name == stationNum)); //builds station
                 if (station.StationId != 0 && drone.DroneId != 0) //if station exists updates it
                 {
