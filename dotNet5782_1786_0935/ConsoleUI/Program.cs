6eb98e588fcfe5df7ab0dc2a6e8fbe0d847106a4
Eliana Grajower <grajower@g.jct.ac.il>
2021-11-02 12:13:53 +0200

    back to the other one

--- dotNet5782_1786_0935/ConsoleUI/Program.cs
+++ dotNet5782_1786_0935/ConsoleUI/Program.cs
@@ -53,7 +53,7 @@ namespace ConsoleUI
                                         Drone d = new Drone() { id = DalObject.r.Next(100000000, 999999999), Model = "Model-" + (Console.ReadLine()) };
                                         Console.WriteLine("Enter maximum weight drone can hold: ");
                                         d.MaxWeight = (WeightCategories)int.Parse(Console.ReadLine());
-                                        d.Battery = 0; //do we want this changed? does the question specify? maybe it should come charged?@@@@@@@@@@@@@@@
+                                        d.Battery = 100; 
                                         d.Status = DroneStatuses.available;
                                         Data.AddDrone(d);
                                         break;
@@ -61,7 +61,7 @@ namespace ConsoleUI
                                 case 'C':
                                     {
                                         Console.WriteLine("Enter name of customer: ");
-                                        Customer c = new Customer() { Name = "Customer-" + (Console.ReadLine()) }; //do we want it like this?@@@@@@@@@@@@
+                                        Customer c = new Customer() { Name = "Customer-" + (Console.ReadLine()) }; 
                                         Console.WriteLine("Enter Id of customer: ");
                                         c.id = int.Parse(Console.ReadLine());
                                         Console.WriteLine("Enter phone number of customer: ");
