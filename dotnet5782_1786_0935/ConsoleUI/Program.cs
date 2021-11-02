0602842d5f53e87be3c154199e411e1c1fed1e8b
Eliana Grajower <grajower@g.jct.ac.il>
2021-11-02 12:51:49 +0200

    cw

--- dotnet5782_1786_0935/ConsoleUI/Program.cs
+++ dotnet5782_1786_0935/ConsoleUI/Program.cs
@@ -77,7 +77,7 @@ namespace ConsoleUI
                                     {
                                         Console.WriteLine("Enter Parcel weight: ");
                                         Parcel p = new Parcel() { id = Data.getParcelId(), Weight = (WeightCategories)int.Parse(Console.ReadLine()), };
-                                        Data.getParcel(p);
+                                        Data.AddParcel(p);
                                         break;
                                     }
                                 default:
@@ -93,7 +93,9 @@ namespace ConsoleUI
                                 "B- collect a parcel by drone\n" +
                                 "C- deliver a parcel to a customer\n" +
                                 "D- charge a drone\n" +
-                                "E- unpug a charging drone");
+                                "E- unpug a charging drone\n" +
+                                "F- find distance to charge station\n"+
+                                "G- find distance from customer");
                             char updateChoice = char.Parse(Console.ReadLine());
                             switch (updateChoice)
                             {
@@ -101,7 +103,7 @@ namespace ConsoleUI
                                     {
                                         Console.WriteLine("Enter parcel id: ");
                                         int parcelId = int.Parse(Console.ReadLine());
-                                        Data.matchUpParcel(Data.findParcel(parcelId));
+                                        Console.WriteLine(Data.matchUpParcel(Data.findParcel(parcelId)) + "\n");
                                         break;
                                     }
                                 case 'B':
@@ -110,7 +112,7 @@ namespace ConsoleUI
                                         int parcelId = int.Parse(Console.ReadLine());
                                         Console.WriteLine("Enter customer id: ");
                                         int customerId = int.Parse(Console.ReadLine());
-                                        Data.pickUpParcel(Data.findCustomer(customerId), Data.findParcel(parcelId));
+                                        Console.WriteLine(Data.pickUpParcel(Data.findCustomer(customerId), Data.findParcel(parcelId))); 
                                         break;
                                     }
                                 case 'C':
@@ -121,7 +123,7 @@ namespace ConsoleUI
                                         int customerId = int.Parse(Console.ReadLine());
                                         Console.WriteLine("Enter level of priority: ");
                                         int priorityLevel = int.Parse(Console.ReadLine());
-                                        Data.deliverParcel(Data.findCustomer(customerId), Data.findParcel(parcelId), priorityLevel);
+                                        Console.WriteLine(Data.deliverParcel(Data.findCustomer(customerId), Data.findParcel(parcelId), priorityLevel)); 
                                         break;
                                     }
                                 case 'D':
@@ -132,14 +134,34 @@ namespace ConsoleUI
                                         Data.printStationsList().ForEach(s => { if (s.ChargeSlots != 0) Console.WriteLine(s.ToString() + "\n"); });
                                         Console.WriteLine("Enter name of station you want to charge drone at:");
                                         stationNum = int.Parse(Console.ReadLine());
-                                        Data.chargeDrone(Data.findDrone(droneId), stationNum);
+                                        Console.WriteLine(Data.chargeDrone(Data.findDrone(droneId), stationNum)); 
                                         break;
                                     }
                                 case 'E':
                                     {
                                         Console.WriteLine("Enter drone id: ");
                                         int droneId = int.Parse(Console.ReadLine());
-                                        Data.releaseDrone(Data.findDroneCharge(droneId));
+                                        Console.WriteLine(Data.releaseDrone(Data.findDroneCharge(droneId))); 
+                                        break;
+                                    }
+                                case 'F':
+                                    {
+                                        Console.WriteLine("Enter langitude coordinates: ");
+                                        double longitutde = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("Enter latitude coordinates: ");
+                                        double latitude= double.Parse(Console.ReadLine());
+                                        Console.WriteLine("the distance is:");
+                                        Data.printStationsList().ForEach(s => { Console.WriteLine(s.Name + ": " + Data.distance(s.Lattitude, s.Longitude, latitude, longitutde)); });
+                                        break;
+                                    }
+                                case 'G':
+                                    {
+                                        Console.WriteLine("Enter langitude coordinates: ");
+                                        double longitutde = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("Enter latitude coordinates: ");
+                                        double latitude = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("the distance is:");
+                                        Data.printCustomersList().ForEach(s => { Console.WriteLine(s.Name + ": " + Data.distance(s.Lattitude, s.Longitude, latitude, longitutde)); });
                                         break;
                                     }
                                 default:
